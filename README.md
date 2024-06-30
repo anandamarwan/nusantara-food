6e780a8366137f4074ec1f9eba342e6f2d1bc507
anandamarwan <anandapuspita2@gmail.com>
2024-06-30 14:08:18 +0700

    feat: create delete, post and put

--- README.md
+++ README.md
@@ -7,14 +7,14 @@ Nusantara Food API for listing various food in Indonesia
 - Production: https://nusantara-food.anandamarwan.com
 - Local: http://localhost:3000
 
-| Endpoint     | HTTP     | Description           |
-| ------------ | -------- | --------------------- |
-| `/foods`     | `GET`    | Get all foods         |
-| `/foods/:id` | `GET`    | Get one food by id    |
-| `/foods`     | `POST`   | Add new food          |
-| `/foods`     | `DELETE` | Delete all foods      |
-| `/foods/:id` | `DELETE` | Delete one food by id |
-| `/foods/:id` | `PUT`    | Update one food by id |
+| Endpoint     | HTTP     | Description           | Implemented |
+| ------------ | -------- | --------------------- | ----------- |
+| `/foods`     | `GET`    | Get all foods         | ✅          |
+| `/foods/:id` | `GET`    | Get one food by id    | ✅          |
+| `/foods`     | `POST`   | Add new food          |             |
+| `/foods`     | `DELETE` | Delete all foods      | ✅          |
+| `/foods/:id` | `DELETE` | Delete one food by id | ✅          |
+| `/foods/:id` | `PUT`    | Update one food by id |             |
 
 ## Getting Started
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { dataFoods } from "./data/food";
+import { type Food, dataFoods } from "./data/food";
 
 let foods = dataFoods;
 
@@ -8,12 +8,18 @@ const app = new Hono();
 app.get("/", (c) => {
   return c.json({
     message: "Nusantara Food API",
-    food: "/foods",
+    foods: "/foods",
   });
 });
 
 app.get("/foods", (c) => {
-  return c.json(dataFoods);
+  if (foods.length <= 0) {
+    return c.json({
+      message: "There is no Foods data",
+    });
+  }
+
+  return c.json({ foods });
 });
 
 app.get("/foods/:id", (c) => {
@@ -21,7 +27,7 @@ app.get("/foods/:id", (c) => {
 
   if (!id) {
     return c.json({
-      massage: "There is no Id",
+      massage: "There is no food Id",
     });
   }
 
@@ -32,6 +38,94 @@ app.get("/foods/:id", (c) => {
       massage: "There is no Food",
     });
   }
+
+  return c.json(food);
+});
+
+app.delete("/foods", (c) => {
+  foods = [];
+
+  return c.json({
+    message: "All foods data have been deleted",
+  });
+});
+
+app.delete("/foods/:id", (c) => {
+  const id = Number(c.req.param("id"));
+
+  if (!id) {
+    return c.json({
+      massage: "There is no food Id",
+    });
+  }
+
+  const food = foods.find((food) => food.id == id);
+
+  if (!food) {
+    return c.json({
+      massage: "There is no food to be deleted",
+    });
+  }
+
+  foods = foods.filter((food) => food.id != id);
+
+  return c.json({
+    message: `Food with Id ${id} has been deleted`,
+
+    deletedFood: food,
+  });
+});
+
+app.post("/foods", async (c) => {
+  const body = await c.req.json();
+
+  const newFood: Food = {
+    id: foods[foods.length - 1].id + 1,
+    name: body.name,
+    category: body.category,
+  };
+
+  const updatedFoods = [...foods, newFood];
+
+  foods = updatedFoods;
+
+  return c.json(newFood);
+});
+
+app.put("/foods/:id", async (c) => {
+  const id = Number(c.req.param("id"));
+
+  if (!id) {
+    return c.json({
+      massage: "There is no food Id",
+    });
+  }
+
+  const food = foods.find((food) => food.id == id);
+
+  if (!food) {
+    return c.json({
+      massage: "There is no Food",
+    });
+  }
+
+  const body = await c.req.json();
+
+  const newFood: Food = {
+    id: food.id,
+    name: body.name,
+    category: body.category,
+  };
+
+  const updatedFoods = foods.map((food) => {
+    if (food.id == id) {
+      return newFood;
+    } else {
+      return food;
+    }
+  });
+
+  foods = updatedFoods;
 
   return c.json(food);
 });
